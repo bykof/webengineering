3995516a73655488d5152d8ced6b9fe726c0b218
Leon Adam <leon.adam.91@gmail.com>
2017-05-18 19:14:21 +0200

    Style entwickeln in Start-Bildschirm und Spiel erstellen

--- frontend/drink/src/App.js
+++ frontend/drink/src/App.js
@@ -1,64 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
 import Start from './components/Start';
 import CreateParty from './components/CreateParty';
 import JoinParty from './components/JoinParty';
 import './App.css';
 
 
+
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      name: "world"
-    };
-    // Notwendig, weil in der Funktion selber "this" genutzt wird
-    // Methode bindet das Objekt an sich
-    // Kann auch in Html geschrieben werden
-    this.changeName = this.changeName.bind(this);
-  }
+    constructor() {
+        super();
+        this.state = {
+            name: "world",
+            nummer: 12
+        };
+        // Notwendig, weil in der Funktion selber "this" genutzt wird
+        // Methode bindet das Objekt an sich
+        // Kann auch in Html geschrieben werden
+        this.changeName = this.changeName.bind(this);
+    }
 
-  changeName(){
-    this.setState({name: "leon"});
-  }
+    changeName() {
+        this.setState({name: "leon"});
+    }
 
-  showValue(value){
-    console.log(value);
-  }
+    showValue(value) {
+        console.log(value);
+    }
 
-  render() {
-    return (
-        /*
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          {
-            // If-Befehl in Html
-          }
-          {
-            this.state.name === 'world' ? (
-                'Welt'
-            ) : (
-                'Keine Welt'
-            )
-          }
-          Hallo {this.state.name}
-          <button onClick={this.changeName}>Klick mich!</button>
-          {
-            // Gibt den Inhalt des Input Fields wieder
-          }
-          <input onChange={(event) => {this.showValue(event.target.value)}}/>
-      </div>
-      */
-        <div>
-          <JoinParty/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route component={Start} path={'/'} exact={true}/>
+                    <Route component={CreateParty} path={'/create-party'}/>
+                    <Route component={JoinParty} path={'/join-party'}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
