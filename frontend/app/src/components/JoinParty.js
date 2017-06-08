8b338560f66cd8acfe507682f143523337a4ce86
Michael Bykovski <mbykovski@seibert-media.net>
2017-06-08 16:50:05 +0200

    OverlayMessage und Error und Joinparty

--- frontend/app/src/components/JoinParty.js
+++ frontend/app/src/components/JoinParty.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import {Link} from 'react-router-dom';
 
 import '../styles/JoinParty.css';
+import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error";
+import OverlayMessage from "./OverlayMessage";
 
 
 export default class JoinParty extends Component {
@@ -14,6 +17,8 @@ export default class JoinParty extends Component {
       entry_code: '',
       name: '',
       is_loading: false,
+      is_error: false,
+      overlay_message: ''
     };
     
     this.updateInputState = this.updateInputState.bind(this);
@@ -37,26 +42,78 @@ export default class JoinParty extends Component {
     ).then(
       (response) => {
         if (response.data.length === 1) {
-          
+          let party = response.data[0];
+          console.log(party);
         } else {
-          
+          this.setState({overlay_message: 'Die Gruppe wurde leider nicht gefunden :('});
+          this.setState({is_loading: false});
         }
       }
     ).catch(
       (error) => {
         console.log(error);
+        this.setState({is_loading: false});
       }
     );
   }
   
   render() {
+    
+    const error = (
+      <Error>
+        <div>
+          <p>Es ist ein Fehler aufgetreten!</p>
+        </div>
+        <div>
+          <button
+            className="primary"
+            onClick={
+              () => {
+                this.setState({is_error: false});
+              }
+            }
+          >
+            OK
+          </button>
+        </div>
+      </Error>
+    );
+    
+    const overlay_message = (
+      <OverlayMessage>
+        <div>
+          <p>{this.state.overlay_message}</p>
+        </div>
+        <div>
+          <button
+            className="primary"
+            onClick={
+              () => {
+                this.setState({overlay_message: ''});
+              }
+            }
+          >
+            OK
+          </button>
+        </div>
+      </OverlayMessage>
+    );
+    
     return (
       <div className="content">
         {
           this.state.is_loading ?
-            <div>
-              <div className="overlay"/>
-            </div> :
+            <LoadingSpinner /> :
+            null
+        }
+        {
+          this.state.is_error ?
+            error :
+            null
+        }
+        {
+          this.state.overlay_message !== '' ?
+            overlay_message :
             null
         }
         <form>
--- frontend/app/src/components/Start.js
+++ frontend/app/src/components/Start.js
@@ -15,9 +15,6 @@ export default class Start extends Component {
     return (
       <div className="content">
         <h1>Drink Olympics</h1>
-        <button onClick={this.getParty.bind(this)}>GetPartys</button>
-        <button onClick={this.getPlayers}>GetPlayers</button>
-        <button onClick={this.delete}>Delete</button>
         <nav className="bar bar-tab">
           <Link to="/create-party" className="tab-item primary">Spiel erstellen</Link>
           <Link to="/join-party" className="tab-item second-primary">Spiel beitreten</Link>
@@ -25,38 +22,5 @@ export default class Start extends Component {
       </div>
     );
   }
-  
-  getParty() {
-    axios.get('http://localhost:8000/api/parties/')
-      .then(function (response) {
-        console.log(response);
-        this.setState({partys: response});
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  
-  getPlayers() {
-    axios.get('http://localhost:8000/api/party-members/')
-      .then(function (response) {
-        console.log(response);
-        this.setState({players: response});
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  
-  delete() {
-    axios.delete('http://localhost:8000/api/party-members/')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  
 }
 
