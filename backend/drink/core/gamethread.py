f1295df7b98897f67df0d0eb45efadfa430d85ab
Michael Bykovski <mbykovski@seibert-media.net>
2017-06-17 18:43:20 +0200

    Aufgeräumt dokumentiert

--- backend/drink/core/gamethread.py
+++ backend/drink/core/gamethread.py
@@ -6,6 +6,15 @@ from async.consumers.party_consumer import PartyConsumer
 
 class GameThread(Thread):
 
+    """
+    Ein GameThread sorgt für den Ablauf der Spiele.
+    Er wählt zufällig ein oder mehrere Spieler aus und zufällig ein Spiel und schickt dies an die PartyMember.
+
+    Regeln:
+    - Ein Spiel dauert maximal MAX_SECONDS Sekunden (damit der GameThread die Leute wieder berücksichtigen kann)
+    - Das wars :)
+
+    """
     def __init__(self, party, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.party = party
@@ -24,7 +33,6 @@ class GameThread(Thread):
         while self.running:
             sleep(1)
             self.tick += 1
-            print(self.party.frontend_game_indexes)
             print('Party: {} is running...'.format(self.party))
             if self.tick == 7:
                 print('Game in {} starts with {}'.format(self.party, self.party_members))
