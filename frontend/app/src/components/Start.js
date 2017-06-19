6e533942cddabbd839aa469914c608ed3fe35c91
UliKo <ulrike.kocher@gmx.net>
2017-06-19 14:27:30 +0200

    Button an Corporate Design angepasst. Zunächst nur auf Startbildschirm.

--- frontend/app/src/components/Start.js
+++ frontend/app/src/components/Start.js
@@ -16,8 +16,8 @@ export default class Start extends Component {
       <div className="content">
         <h1>Drink Olympics</h1>
         <nav className="bar bar-tab">
-          <Link to="/create-party" className="tab-item primary">Spiel erstellen</Link>
-          <Link to="/join-party" className="tab-item second-primary">Spiel beitreten</Link>
+          <Link to="/create-party" className="btn primary">Spiel erstellen</Link>
+          <Link to="/join-party" className="btn second-primary">Spiel beitreten</Link>
         </nav>
       </div>
     );
