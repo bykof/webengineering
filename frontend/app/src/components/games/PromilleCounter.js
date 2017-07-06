9d539a611d5dcf90d386ad37a232be97c3401667
UliKo <ulrike.kocher@gmx.net>
2017-07-06 17:03:56 +0200

    Spiel so weit fertig.

--- frontend/app/src/components/games/PromilleCounter.js
+++ frontend/app/src/components/games/PromilleCounter.js
@@ -4,10 +4,14 @@ import React from 'react';
 
 export default class PromilleCounter extends GameComponent {
 
+  static description = 'Blase so lang und fest ins Mikrofon wie möglich!';
+
   constructor(props) {
     super(props);
     this.state = {
-      audioData: null
+      audioData: null,
+      maxValue: [],
+      members: []
     };
 
     this.onAudioInput = this.onAudioInput.bind(this);
@@ -25,18 +29,57 @@ export default class PromilleCounter extends GameComponent {
   }
 
   onAudioInput(event) {
-    console.log("Audio data received: " + event.data.length + " samples");
     this.setState({audioData: event.data});
+
+    let max = 0;
+    // Höchster Wert in audioData
+    for (let i = 0; i < this.state.audioData.length; i++) {
+      if (max < this.state.audioData[i]) {
+        max = this.state.audioData[i];
+      }
+    }
+    //Math.min.apply(Math, this.state.audioData);
+    this.state.maxValue.push(max);
+    console.log("Max: " + max);
+
+    this.socket.send(
+      {
+        party_member: this.props.application_store.current_member,
+        maxList: this.state.maxValue
+      });
   }
 
   // Wird aufgerufen, wenn eine Nachricht über this.socket.send versendet wurde
   onDefaultMessage(data) {
+    this.state.members[data.party_member.name] = data.maxList;
   }
 
   // Wird aufgerufen wenn das Spiel vorbei ist (nach 15 Sekunden)
   onGameFinished() {
     audioinput.stop();
-    super.onGameFinished(true);
+    let sum = 0;
+    let endValue;
+    let winner = ["default", 0];
+
+    for (let member in this.state.members) {
+      for (let i = 0; i < this.state.members[member].length; i++) {
+        sum += this.state.maxValue[i];
+      }
+      endValue = sum / this.state.maxValue.length;
+      if (endValue > winner[1]) {
+        winner = [member, endValue]
+      }
+    }
+    console.log(winner);
+
+    if (winner[0] === this.props.application_store.current_member.name) {
+      super.onGameFinished(true);
+      console.log("Gewinner");
+    } else {
+      super.onGameFinished(false);
+      console.log("Verlierer");
+    }
+
   }
 
   // Wird aufgerufen bei jeder Sekunde (15 Mal)
@@ -47,13 +90,14 @@ export default class PromilleCounter extends GameComponent {
     return (
       <div>
         <h1>Promille Zähler</h1>
-        {this.state.audioData ? (
-          this.state.audioData.map(
-            (data) => {
-              return <span>{data}</span>
-            }
-          )
-        ) : null}
+        {//this.state.audioData ? (
+          //  this.state.audioData.map(
+          //    (data) => {
+          //      return <p>{data}</p>
+          //    }
+          //  )
+          //) : null
+        }
       </div>
     )
   }
