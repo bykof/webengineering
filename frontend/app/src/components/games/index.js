f18fbd262e5fec4f8eebd8e62a3a056b5406028c
UliKo <ulrike.kocher@gmx.net>
2017-07-13 16:36:00 +0200

    FastSwiping raus genommen

--- frontend/app/src/components/games/index.js
+++ frontend/app/src/components/games/index.js
@@ -1,5 +1,4 @@
 import FastTipping from './FastTipping';
-import FastSwiping from './FastSwiping';
 import ShovelGold from "./ShovelGold";
 import BubblePop from './BubblePop';
 import PromilleCounter from './PromilleCounter';
@@ -7,7 +6,6 @@ import PromilleCounter from './PromilleCounter';
 
 export default {
   fast_tipping: FastTipping,
-  fast_swiping: FastSwiping,
   bubble_pop: BubblePop,
   promille_counter: PromilleCounter,
   shovel_gold: ShovelGold
